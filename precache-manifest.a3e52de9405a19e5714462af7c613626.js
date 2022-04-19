42a5bb789fc63611e33042bcb05ae500e001a2f7
Ke-vin Chan <chan9367@gmail.com>
2022-04-19 14:25:50 -0500

    Updates

---
+++ precache-manifest.a3e52de9405a19e5714462af7c613626.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e63752acdca96ba13bb3437d9cbd379",
+    "url": "/IVAsearch/index.html"
+  },
+  {
+    "revision": "c167c7b3830ac18d2b54",
+    "url": "/IVAsearch/static/css/main.8708cf50.chunk.css"
+  },
+  {
+    "revision": "887edeab135448aa0cef",
+    "url": "/IVAsearch/static/js/2.d4e90398.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/IVAsearch/static/js/2.d4e90398.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c167c7b3830ac18d2b54",
+    "url": "/IVAsearch/static/js/main.6af7ecaf.chunk.js"
+  },
+  {
+    "revision": "7ec0bf1e5782eb55e1ee",
+    "url": "/IVAsearch/static/js/runtime-main.51dfab1e.js"
+  }
+]);
